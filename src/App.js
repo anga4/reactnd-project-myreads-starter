1ec1c9aa2fb7693864ad8917de3bb240543d5da2
anga4 <anga4brandon500@gmail.com>
2020-04-19 12:01:25 +0100

    Refactor: Updated code to display books from API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { Route } from "react-router-dom";
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import MainPage from "./MainPage";
 import SearchPage from "./SearchPage";
 
 class BooksApp extends React.Component {
-    state = {};
+    state = {
+        books: null
+    };
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState(
+                () => ({
+                    books
+                }),
+                console.log("Books", books)
+            );
+        });
+    }
 
     render() {
+        const { books } = this.state;
         return (
             <div className="app">
-                <Route exact path="/" render={() => <MainPage />} />
-                <Route exact path="/search" render={() => <SearchPage />} />
+                <Route exact path="/" render={() => <MainPage books={books} />} />
+                <Route exact path="/search" render={() => <SearchPage books={books} />} />
             </div>
         );
     }
